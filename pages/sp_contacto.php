2501abd88aaeaa58dc4735933432bd58020cbddb
Sergio Crúz Flores <scruz@fmf.mx>
2024-11-15 16:15:06 -0600

    Se suben ajustes del Fri

--- pages/sp_contacto.php
+++ pages/sp_contacto.php
@@ -16,6 +16,7 @@
       <link  rel="stylesheet" href="/serflix/assets/css/components/contacto.css">
       <link  rel="stylesheet" href="/serflix/assets/css/components/breadcrums.css">
       <link  rel="stylesheet" href="/serflix/assets/css/components/footer.css">
+      <link  rel="stylesheet" href="/serflix/assets/css/components/cookies.css">
       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" >
   </head>  
   <body>
